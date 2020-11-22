937483e645a82d0df445eda7c7d56b52cb6f35fb
adiswami14 <adithya1431@gmail.com>
2020-11-21 21:58:58 -0500

    Add a status update up top and a restart button

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-/*----- constants -----*/
-
-
-/*----- app's state (variables) -----*/
 let board;
 let currentTurn = 'X';
-/*----- cached element references -----*/
+let winner;
 const squares = Array.from(document.querySelectorAll("div.square"));
-/*----- event listeners -----*/
+const topMessage = document.querySelector('h2');
 document.getElementById("board").addEventListener("click", makeTurn);
-/*----- functions -----*/
 function init() {
+    topMessage.textContent = "Start the game up!";
     board = [
         '', '', '',
         '', '', '',
@@ -37,9 +33,50 @@ function makeTurn(event) {
     if(event.target.innerHTML === "") { //square has not been clicked yet
         board[index] = currentTurn;
         event.target.innerHTML = currentTurn;
+        winner = checkWin(currentTurn);
         currentTurn === 'X' ? currentTurn = 'O' : currentTurn = 'X';
+        if(winner === '') {
+            topMessage.textContent = `It is ${currentTurn}'s turn!`
+        } else topMessage.textContent = `${winner} won!`
     }
-    console.log(board);
+}
+
+function checkWin(currChar) {
+    if(checkWinByRows(currChar) || checkWinByColumns(currChar) || checkWinByDiagonals(currChar)) {
+        return currChar;
+    }
+    return '';
+}
+
+function checkWinByRows(currChar) {
+    if(currChar === board[0] && currChar === board[1] && currChar === board[2]) { //row one
+        return true;
+    } else if(currChar === board[3] && currChar === board[4] && currChar === board[5]) { //middle row
+        return true;
+    } else if(currChar === board[6] && currChar === board[7] && currChar === board[8]) { //last row
+        return true;
+    }
+    return false;
+}
+
+function checkWinByColumns(currChar) {
+    if(currChar === board[0] && currChar === board[3] && currChar === board[6]) { //column one
+        return true;
+    } else if(currChar === board[1] && currChar === board[4] && currChar === board[7]) { //middle column
+        return true;
+    } else if(currChar === board[2] && currChar === board[5] && currChar === board[8]) { //last column
+        return true;
+    }
+    return false;
+}
+
+function checkWinByDiagonals(currChar) {
+    if(currChar === board[0] && currChar === board[4] && currChar === board[8]) { //diagonal
+        return true;
+    } else if(currChar === board[2] && currChar === board[4] && currChar === board[6]) { //anti-diagonal
+        return true;
+    } 
+    return false;
 }
 
 init();
